7846f86d72101ec5be6b4329d6fc07892271d6ff
Kishor Laskar <30683532+kishorlaskar@users.noreply.github.com>
2020-11-04 00:25:02 +0600

    quize_updated
    quize_updated

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/','WelcomeController@index')->name('/');
+
+Auth::routes();
+
+Route::get('/from-validate',function()
+{
+    return view('from-validate');
+});
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('next-question','NextController');
+Route::get('/score/score-detail', 'MainController@detail')
+    ->name('score.detail');
+Route::get('/quiz','MainController@next')->name('quiz');
+Route::any('start-quiz','MainController@startquiz')->name('start-quiz');
+Route::any('submit-ans','MainController@submitans')->name('submit-ans');
+Route::get('/leadboard','MainController@leadboard')->name('leadboard');
+
+Route::resource('topics', 'TopicController');
+Route::resource('questions', 'QuestionController');
+Route::resource('results', 'ResultsController');
+Route::resource('adminpanel', 'AdminPanelController');
+Route::resource('questionsOptions', 'QuestionsOptionsController');
+Route::resource('users', 'UserController');
